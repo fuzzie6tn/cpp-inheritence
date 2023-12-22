d247c9583e0f70f4ed5bdf742fb08b1f07ba928e
Fazila Roshan Beg <fazilaroushanbeg@gmail.com>
2023-12-22 22:01:00 +0500

    commit

---
+++ inheritance/file exception/classtask.cpp
@@ -0,0 +1,8 @@
+/*
+Define a custom exception class named SecureArray
+having array of int type as single data member and let the insert
+and delete functions of this class to throw OutBoundException
+(your own defined exception class) if the array is already full
+or already empty, respectively. In the main() function, write a try-catch
+block to handle this type of exception.
+*/
